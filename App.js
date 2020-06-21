04776dbe44477f9b8093a93d7aef6594a3824574
Jan Co Chiong <jan.cochiong@gmail.com>
2020-06-21 01:55:58 -0400

    fix fonts

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
-import * as Font from "expo-font";
+import { useFonts } from "@use-expo/font";
 import { AppLoading } from "expo";
 
 import Header from "./components/Header";
@@ -8,27 +8,17 @@ import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
 
-const fetchFonts = () => {
-  Font.loadAsync({
-    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
-  });
-};
-
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
 
-  const [dataLoaded, setDataLoaded] = useState(false);
+  let [fontsLoaded] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
 
-  if (!dataLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setDataLoaded(true)}
-        onError={(err) => console.log(err)}
-      />
-    );
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
 
   const configureNewGameHandler = () => {
